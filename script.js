b0f38730dd1587005af849ec6740243af60f4de6
Misha <mykhailo.bondar1@gmail.com>
2025-09-22 09:38:11 +0200

    changed js code for attempts

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ function startGame() {
     maxAttempts = 5;
   }
 
-  secretNumber = Math.floor(Math.random() * 100) + 1;
+  secretNumber = Math.floor(Math.random() * maxNumber) + 1;
   attempts = 0;
+
+  document.querySelector(".game-container p").textContent = `Guess a number between 1 and ${maxNumber}. You have ${maxAttempts} attempts.`;
+
   document.getElementById("feedback").textContent = "";
   document.getElementById("attempts").textContent = "";
   document.getElementById("guessInput").value = "";
@@ -28,14 +31,17 @@ document.getElementById("submitGuess").addEventListener("click", function() {
   const userGuess = Number(document.getElementById("guessInput").value);
   attempts++;
 
-  if (!userGuess || userGuess < 1 || userGuess > 100) {
-    document.getElementById("feedback").textContent = "⚠️ Please enter a number between 1 and 100!";
+  if (!userGuess || userGuess < 1 || userGuess > maxNumber) {
+    document.getElementById("feedback").textContent = "⚠️ Please enter a number between 1 and ${";
     return;
   }
 
   if (userGuess === secretNumber) {
     document.getElementById("feedback").textContent = `YAY! Correct! The number was ${secretNumber}. Maybe try again?`;
     document.getElementById("feedback").style.color = "green";
+  }else if (attempts >= maxAttempts) {
+    document.getElementById("feedback").textContent =
+    "Game over! The number was ${secretNumber}! Try again?";
   } else if (userGuess < secretNumber) {
     document.getElementById("feedback").textContent = "Naah... Too low! Try again.";
     document.getElementById("feedback").style.color = "red";
@@ -44,7 +50,7 @@ document.getElementById("submitGuess").addEventListener("click", function() {
     document.getElementById("feedback").style.color = "red";
   }
 
-  document.getElementById("attempts").textContent = `Attempts: ${attempts}`;
+  document.getElementById("attempts").textContent = `Attempts: ${attempts} / ${maxAttempts}`;
 });
 
 document.getElementById("restartGame").addEventListener("click", startGame);
